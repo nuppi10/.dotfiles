aacb978b29832245094c8f3b1bb0b8307b79b897
nuppi10 <nubbi.nabbi@proton.me>
2026-02-10 09:02:20 +0200

    lot of changes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
-return {
-
-        "bluz71/vim-moonfly-colors", 
-        name = "moonfly", 
-        lazy = false, priority = 1000,
-        config = function()
-            vim.g.moonflyTransparent = true
-            vim.cmd("colorscheme moonfly")
-        end
-
-}
+return     {
+      'sainnhe/gruvbox-material',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.gruvbox_material_enable_italic = true
+        vim.cmd.colorscheme('gruvbox-material')
+      end
+    }
